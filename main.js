2d368ae8ff291d0239a5c1c1a4755e8b52720e6e
Son Dong Chan <channi.kr@gmail.com>
2020-03-09 16:42:07 +0900

    a

---
+++ main.js
@@ -0,0 +1,84 @@
+//배열 메소드
+
+// var arr = [1,2,3,4];
+// arr.pop();
+// console.log(arr);
+
+// var arr = [1,2,3,4];
+// arr.push(5);
+// console.log(arr);
+
+// var arr = [1,2,3,4];
+// arr.unshift(0);
+// console.log(arr);
+
+// var arr = [1,2,3,4];
+// arr.shift();
+// console.log(arr);
+
+// var arr = [1,2,3,4,5,6,7];
+// arr.splice(3,2);
+// console.log(arr);
+
+// var arr = [1,2,3,4,5,6,7];
+// arr.splice(2,1,"a","b");
+// console.log(arr);
+
+// var arr = [1,2,3,4,5,6,7];
+// var newArr = arr.slice(3,6);
+// console.log(newArr);
+
+// var arr1 = [1,2,3];
+// var arr2 = [4,5,6];
+// var arr3 = arr2.concat(arr1);
+// console.log(arr3);
+
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var isEven = function (value) {
+// 	return value % 2 === 0;
+// };
+// console.log(arr.every(isEven));
+
+// var arr = [1,2,3];
+// arr.forEach(function(value) {
+// 	console.log(value);
+// })
+
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var isEven = function (value) {
+// 	return value % 2 === 0;
+// }
+// var newArr = arr.map(isEven);
+// var newArr2 = arr.filter(isEven);
+// console.log(newArr);
+// console.log(newArr2);
+
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var value = arr.reduce(function (previousValue, currentValue, index) {
+// 	return previousValue + currentValue;
+// });
+// console.log(value);
+
+// var arr = [1, 2, 3, 4];
+// arr.reverse();
+// console.log(arr);
+
+// var arr = [13, 12, 11, 10, 5, 3, 2, 1];
+// arr.sort();
+// console.log(arr);
+
+// var arr = [13, 12, 11, 10, 5, 3, 2, 1];
+// arr.sort(function(a,b) {
+// 	return a-b;
+// })
+// console.log(arr)
+
+// var arr = [1, 2, 3, 4];
+// console.log(arr.toString());
+
+// var arr = [1,2,3,4];
+// console.log(arr.valueOf());
+
+// var arr = [1, 2, 3, 4];
+// console.log(arr.join());
+// console.log(arr.join('+'));
