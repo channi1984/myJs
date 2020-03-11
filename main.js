fcc4a8a1776d1e26b119dd2b0926b6e244d71318
Son Dong Chan <channi.kr@gmail.com>
2020-03-11 17:30:17 +0900

    a

--- main.js
+++ main.js
@@ -144,12 +144,12 @@
 // console.log(myArray);
 // console.log(myEvenArray);
 
-const myArray = [1,2,3,4];
+// const myArray = [1,2,3,4];
 
-const sum =myArray.reduce((acc, currValue, currIndex, array) => {
-    return acc + currValue;
-}, 0);
+// const sum =myArray.reduce((acc, currValue, currIndex, array) => {
+//     return acc + currValue;
+// }, 0);
 
-const avg = sum / myArray.length;
+// const avg = sum / myArray.length;
 
-console.log(avg);
+// console.log(avg);
